1f8d79008bd5761501364d61ac57559112762de5
shgnaka <shogonakamurawppt@gmail.com>
2025-04-16 16:26:10 +0900

    feat: add LinearSearch class and search method

--- 01/ex01/LinearSearch.java
+++ 01/ex01/LinearSearch.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 public class LinearSearch {
-
+    
+    static final int NUM = 11;
+    static int a[]= {0, 1, 8, 27, 64, 125, 216, 343, 512, 729, 1000};
+    int key;
     public static void main() {
-        
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("整数を入力してください: ");
+            int key = sc.nextInt();
+            search(key);
+            System.out.println("index" + search(key));
+            if (key == -999999) {
+                break;
+            }
+        }
     }
     public static int search(int key) {
-        
+        for (int i = 0; i < NUM; i++) {
+            if (a[i] == key) {
+                return i;                 
+            }
+        }
+        return -1;
     }
 }

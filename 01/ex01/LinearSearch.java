18e29b8d81e88de8002d8a1f0f5b6d864714bd46
shgnaka <shogonakamurawppt@gmail.com>
2025-04-16 17:09:45 +0900

    fix: change value of NUM variable and a[] list

--- 01/ex01/LinearSearch.java
+++ 01/ex01/LinearSearch.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class LinearSearch {
     
-    static final int NUM = 4;
-    static int a[]= {4096, 512, 16, 1};
+    static final int NUM = 6;
+    static int a[]= {2005, 0412, 00, 1, 830,1102};
     int key;
     public static void main(String args[]) {
         while(true) {

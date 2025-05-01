1e00ef38a13e8368c7b3f977bc45b4131f532268
shgnaka <shogonakamurawppt@gmail.com>
2025-05-01 11:47:03 +0900

    feat: complete main method in Ex3_21 class

--- 03/ex3/Ex3_21.java
+++ 03/ex3/Ex3_21.java
@@ -1,25 +1,33 @@
 public class Ex3_21 {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack(100);
 
-    MyStack stack = new MyStack(100);
+        String c = "1 2 + 3 4 - *";
 
-    String c = "1 2 + 3 4 - *";
-
-    MyTokenList tokenList = new MyTokenList(c);
+        MyTokenList tokenList = new MyTokenList(c);
 
     for(int i = 0; i < tokenList.size(); i++) {
         MyToken t = tokenList.get(i);
         if (t.isOperator()) {
-            int num2 = stack.pop();
             int num1 = stack.pop();
-            t の演算子に応じて演算をし，結果を push() する
+            int num0 = stack.pop();
+            if (t.getOperator() == '+') {
+                stack.push(num0 + num1);
+            } else if (t.getOperator() == '-') {
+                stack.push(num0 - num1);
+            } else if (t.getOperator() == '*') {
+                stack.push(num0 * num1);
+            } else if (t.getOperator() == '/') {
+                stack.push(num0 / num1);
+            } else if (t.getOperator() == '%') {
+                stack.push(num0 % num1);
+            }
+            //t の演算子に応じて演算をし，結果を push() する
         } else if (t.isDigit()) {
-            t の数値を push() する
+            stack.push(t.getDigit());
         }
     }
 
     System.out.println("result: " + stack.pop());
-
-    public static void main(String[] args) {
-        Ex3_21 ex = new Ex3_21();
     }
 }
